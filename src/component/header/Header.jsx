4e7382ea48adda79b2050c93696b72339a79e330
mtae616 <mtae616@naver.com>
2022-11-08 16:05:55 +0900

    add header component

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,26 @@
+import * as s from './Header.style'
+import Button from "../../element/Button";
+import {useNavigate} from 'react-router-dom'
+
+const Header = () => {
+    const navigate = useNavigate()
+    return (
+        <s.GridBox>
+            <div
+                onClick={() => navigate(`/`)}
+            >ola</div>
+            <s.SideBox>
+                <Button
+                    padding={"10px"}
+                    _onClick={() => navigate('/signup')}
+                >회원가입</Button>
+                <Button
+                    padding={"10px"} margin={"0px 0px 0px 10px"}
+                    _onClick={() => navigate('/login')}
+                >로그인</Button>
+            </s.SideBox>
+        </s.GridBox>
+    )
+}
+
+export default Header
