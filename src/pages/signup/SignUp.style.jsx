b2032dedf66b8950225c07568dfc16602600c8f4
mtae616 <mtae616@naver.com>
2022-11-08 22:47:47 +0900

    add sign up style

---
+++ src/pages/signup/SignUp.style.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const GridBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 16px;
+    background: #f5f6f7;
+`
+
+export const SelectContainer = styled.div`
+    width: 100%;
+`
+
+export const SelectBox = styled.fieldset`
+    outline: 1px solid black;
+    border-radius: 5px;
+    background: white;
+    display: grid;
+    margin: 10px 0px;
+`
+
+export const RadioBox = styled.div`
+    padding: 16px;
+`
