c05348c4b9f4c29bce50c51d1b3033c4898c66c7
mtae616 <mtae616@naver.com>
2022-11-08 22:48:04 +0900

    add write style

---
+++ src/pages/write/Write.style.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const GridBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background: #f5f6f7;
+    padding: 16px 0px;
+`
+
+export const ButtonBox = styled.div`
+    display: flex;
+`
+
+export const SelectContainer = styled.div`
+    width: 100%;
+`
+
+export const SelectBox = styled.fieldset`
+    outline: 1px solid black;
+    border-radius: 5px;
+    background: white;
+    display: grid;
+    margin: 10px 0px;
+`
+
+export const RadioBox = styled.div`
+    padding: 16px;
+`
