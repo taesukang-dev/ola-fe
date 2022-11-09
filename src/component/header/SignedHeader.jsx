43b2089bd56ebb7e974d95194618a3833991542e
mtae616 <mtae616@naver.com>
2022-11-09 16:24:25 +0900

    add logout button

--- src/component/header/SignedHeader.jsx
+++ src/component/header/SignedHeader.jsx
@@ -1,9 +1,14 @@
 import * as s from "./Header.style";
 import Button from "../../element/Button";
 import {useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {deleteCookie} from "../../shared/Cookie";
+import {setUserUp} from "../../store/userSlice";
 
 const SignedHeader = () => {
+    const dispatch = useDispatch()
     const navigate = useNavigate()
+    const user = useSelector((state) => state.user)
     return (
         <s.GridBox>
             <div
@@ -15,8 +20,16 @@ const SignedHeader = () => {
                 >내정보</Button>
                 <Button
                     padding={"10px"} margin={"0px 0px 0px 10px"}
-                    _onClick={() => navigate('/team/write')}
+                    _onClick={() => navigate('/write/team')}
                 >남는거 ㅋㅋ</Button>
+                <Button
+                    padding={"10px"} margin={"0px 0px 0px 10px"}
+                    _onClick={() => {
+                        deleteCookie('x_auth')
+                        dispatch(setUserUp(''))
+                        navigate('/')
+                    }}
+                >로그아웃</Button>
             </s.SideBox>
         </s.GridBox>
     )
