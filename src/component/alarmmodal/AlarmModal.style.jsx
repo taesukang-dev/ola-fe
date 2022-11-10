49dc0fe30a339b44bceaa3d151b8e4799f64ff69
mtae616 <mtae616@naver.com>
2022-11-10 22:48:58 +0900

    add alarm modal style

---
+++ src/component/alarmmodal/AlarmModal.style.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const AlarmModal = styled.div`
+    position: fixed;
+    background: #04cf5b;
+    padding: 16px;
+    right: 70px;
+    border-radius: 10px;
+`
+
+export const AlarmColumn = styled.div`
+    padding: 10px 0px;
+    color: white;
+    font-weight: bold;
+`
