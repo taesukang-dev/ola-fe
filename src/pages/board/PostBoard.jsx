05618d9cd9b5cd608c44cf7ab4cca8702d45073f
mtae616 <mtae616@naver.com>
2022-11-12 15:26:07 +0900

    add paging button

--- src/pages/board/PostBoard.jsx
+++ src/pages/board/PostBoard.jsx
@@ -4,19 +4,29 @@ import {getPostList} from "../../shared/api/api";
 import PostList from "../../component/postlist/PostList";
 import Button from "../../element/Button";
 import {useNavigate} from "react-router-dom";
+import PageButton from "../../component/pagebutton/PageButton";
+import {useSelector} from "react-redux";
+import {useEffect} from "react";
 
 const PostBoard = () => {
     const navigate = useNavigate()
-    const { data } = useQuery(['postList'], () => getPostList())
+    const page = useSelector((state) => state.page)
+    const { data, refetch } = useQuery(['postList'], () => getPostList(page.post))
+
+    useEffect(() => {
+        refetch()
+    }, [page])
+
     return (
         <s.GridContainer>
-            {data && <PostList postList={data?.result} />}
+            {data && <PostList postList={data?.result[0]} />}
             <s.GridBox>
                 <Button
                     padding={"10px"}
                     _onClick={() => navigate("/write")}
                 >글작성</Button>
             </s.GridBox>
+            {data && <PageButton buttonList={data?.result[1]} type={"post"}/>}
         </s.GridContainer>
     )
 }
--- src/pages/board/TeamPostBoard.jsx
+++ src/pages/board/TeamPostBoard.jsx
@@ -4,20 +4,29 @@ import {getTeamPostList} from "../../shared/api/api";
 import TeamPostList from "../../component/postlist/TeamPostList";
 import Button from "../../element/Button";
 import {useNavigate} from "react-router-dom";
+import PageButton from "../../component/pagebutton/PageButton";
+import {useSelector} from "react-redux";
+import {useEffect} from "react";
 
 const TeamPostBoard = () => {
     const navigate = useNavigate()
-    const {data} = useQuery(['teamPostList'], () => getTeamPostList())
+    const page = useSelector((state) => state.page)
+    const {data, refetch} = useQuery(['teamPostList'], () => getTeamPostList(page.teamPost))
+
+    useEffect(() => {
+        refetch()
+    }, [page])
 
         return (
             <s.GridContainer>
-                {data && <TeamPostList teamPostList={data.result}/>}
+                {data && <TeamPostList teamPostList={data.result[0]}/>}
                 <s.GridBox>
                     <Button
                         padding={"10px"}
                         _onClick={() => navigate("/write/team")}
                     >글작성</Button>
                 </s.GridBox>
+                {data && <PageButton buttonList={data?.result[1]} type={"teamPost"}/>}
             </s.GridContainer>
         )
 }
