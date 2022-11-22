3b656bac739d1cc31878c5f5a9e3f3c7252b8451
mtae616 <mtae616@naver.com>
2022-11-22 21:59:35 +0900

    add user position

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -2,14 +2,24 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const user = createSlice({
     name: 'user',
-    initialState: {current: ''},
+    initialState: {
+        current: '',
+        position: {
+            x: 0,
+            y: 0
+        }
+    },
     reducers: {
         setUserUp(state, action) {
             state.current = action.payload
         },
+        setUserPosition(state, action) {
+            state.position.x = action.payload.x
+            state.position.y = action.payload.y
+        },
     }
 })
 
-export const {setUserUp} = user.actions
+export const {setUserUp, setUserPosition} = user.actions
 
 export default user
