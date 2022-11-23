5e5eb0ca2e514785e74dee6a52f53b5521b74c6d
mtae616 <mtae616@naver.com>
2022-11-23 21:42:43 +0900

    add kakao search api component

---
+++ src/component/searchhomegym/SearchHomeGym.style.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const SearchBox = styled.div`
+`
+
+export const SearchItemContainer = styled.div`
+    height: 15vh;
+    border: 1px solid black;
+    overflow: scroll;
+`
+
+export const SearchItemBox = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+`
+
+export const TextBox = styled.div`
+    margin: 10px 0px;
+`
