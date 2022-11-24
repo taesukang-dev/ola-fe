66cd06d9356b3405c96bc733e5a64b13f3e9e669
mtae616 <mtae616@naver.com>
2022-11-24 16:54:57 +0900

    add condition when it is locationPost

--- src/component/pagebutton/PageButton.jsx
+++ src/component/pagebutton/PageButton.jsx
@@ -1,6 +1,6 @@
 import Button from "../../element/Button";
 import {useDispatch, useSelector} from "react-redux";
-import {setPostPage, setTeamPostPage} from "../../store/pageSlice";
+import {setLocationPostPage, setPostPage, setTeamPostPage} from "../../store/pageSlice";
 
 const PageButton = ({buttonList, type}) => {
     const dispatch = useDispatch()
@@ -9,28 +9,58 @@ const PageButton = ({buttonList, type}) => {
     const handlePage = (e) => {
         if (type === "post") {
             dispatch(setPostPage(e));
-        } else {
+        } else if (type === 'teamPost') {
             dispatch(setTeamPostPage(e))
+        } else if (type === 'locationPost') {
+            dispatch(setLocationPostPage(e))
         }
     }
-
-    return (
-        <div>
-            {
-                buttonList.map((e, i) =>
-                    <Button
-                        key={i}
-                        margin={"0px 1px"}
-                        _onClick={() => handlePage(e)}
-                        warn={ type === "post"
-                                ? e === page.post ? "true" : "false"
-                                : e === page.teamPost ? "true" : "false"}
-                    >{e + 1}
-                    </Button>
-                )
-            }
-        </div>
-    )
+    if (type === "post") {
+        return (
+            <div>
+                {
+                    buttonList.map((e, i) =>
+                        <Button
+                            key={i}
+                            margin={"0px 1px"}
+                            _onClick={() => handlePage(e)}
+                            warn={e === page.post ? "true" : "false"}
+                        >{e + 1}
+                        </Button>
+                    )
+                }
+            </div>)
+    } else if (type === 'teamPost') {
+        return (
+            <div>
+                {
+                    buttonList.map((e, i) =>
+                        <Button
+                            key={i}
+                            margin={"0px 1px"}
+                            _onClick={() => handlePage(e)}
+                            warn={e === page.teamPost ? "true" : "false"}
+                        >{e + 1}
+                        </Button>
+                    )
+                }
+            </div>)
+    } else if (type === 'locationPost') {
+        return (
+            <div>
+                {
+                    buttonList.map((e, i) =>
+                        <Button
+                            key={i}
+                            margin={"0px 1px"}
+                            _onClick={() => handlePage(e)}
+                            warn={e === page.locationPost ? "true" : "false"}
+                        >{e + 1}
+                        </Button>
+                    )
+                }
+            </div>)
+    }
 }
 
 export default PageButton
